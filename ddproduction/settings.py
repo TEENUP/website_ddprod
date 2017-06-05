b60df8dc2abaf900b0fd974017fcc5362cfd19be
TEENUP <puneetgupta5294@gmail.com>
2017-06-05 16:32:24 +0530

    update settings

--- ddproduction/settings.py
+++ ddproduction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0@b@#6jnix=m*ta5gcew*0@ns8=6ol0te3*#kk4o%ma4=ei$6g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , '.pythonanywhere.com' ]
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
