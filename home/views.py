d319b87e0a7f4701b72957b33412a8af37503c5b
rishabhcsi <rishabh.750398@gmail.com>
2017-06-07 17:53:23 +0530

    Add URLs

--- home/views.py
+++ home/views.py
@@ -6,4 +6,17 @@ from django.shortcuts import render
 def home_list(request):
     return render(request, 'home/index.html', {})
 
+def contact_us(request):
+    return render(request, 'home/contact_us.html', {})
+
+def sign_up(request):
+    return render(request, 'home/sign_up.html', {})
+
+def about_us(request):
+    return render(request, 'home/about_us.html', {})
+
+def dashboard(request):
+    return render(request, 'home/dashboard.html', {})
+
+
 # Create your views here.
