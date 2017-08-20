cbc15ed94617700941e6d30301e1885011d9e3d1
TEENUP <puneetgupta5294@gmail.com>
2017-08-20 23:27:49 +0530

    search_fields in administration

--- home/admin.py
+++ home/admin.py
@@ -13,14 +13,42 @@ from .models import UserRefferal
 
  
 # Register your models here.
+class userAdmin(admin.ModelAdmin):
+	#date_hierarchy = 'timestamp' #updated
+	search_fields = ['username']
+	#list_display = ['title','price','active','updated']
+	#list_editable = ['price', 'active']
+	#list_filter = ['price','active']
+	#readonly_fields = ['updated','timestamp']
+	#prepopulated_fields = {"slug": ("title",)}
+	# class Meta:
+	# 	model = User
+
+class userAccountsAdmin(admin.ModelAdmin):
+	search_fields = ['username','sponserId']
+
+class userDetailsAdmin(admin.ModelAdmin):
+	search_fields = ['username']
+
+class userRefferalAdmin(admin.ModelAdmin):
+	search_fields = ['sponserId', 'username']
+
+class userRelationAdmin(admin.ModelAdmin):
+	search_fields = ['childUsername','parentUsername']
+
+
+
+
+
+
 admin.site.site_header = 'Petals Art Jewellery Administration'
-admin.site.register(User)	
-admin.site.register(UserDetails)
-admin.site.register(UserAccount)
-admin.site.register(UserRelation)
+admin.site.register(User, userAdmin)	
+admin.site.register(UserDetails, userDetailsAdmin)
+admin.site.register(UserAccount, userAccountsAdmin)
+admin.site.register(UserRelation, userRelationAdmin)
 admin.site.register(Product)
 admin.site.register(SpecialProduct)
 admin.site.register(ProductImage)
-admin.site.register(UserRefferal)
+admin.site.register(UserRefferal, userRefferalAdmin)
 
 
