10c90398ad843fcac8048ab581ec9bb1da5f75a6
TEENUP <puneetgupta5294@gmail.com>
2017-09-14 17:10:56 +0530

    indentation error

--- home/views.py
+++ home/views.py
@@ -992,40 +992,40 @@ def buyProducts(request):
 
 
 		p_merchant_id = "147110"
-			p_order_id = product.productId
-			p_currency = "INR"
-			p_amount = product.price
-			p_redirect_url = "https://www.petalsart.in/ccavResponseHandler/"
-			p_cancel_url = "https://www.petalsart.in/ccavResponseHandler/"
-			p_language = "EN"
-			p_billing_name = "userDetails.firstName" + ' ' + "userDetails.lastName"
-			p_billing_address = "userDetails.address"
-			p_billing_city = "Delhi"
-			p_billing_state = "Delhi"
-			p_billing_zip = "110085"
-			p_billing_country = "India"
-			#p_billing_tel = userDetails.phoneNo
-			p_billing_email = "userDetails.email"
-			p_delivery_name = "userDetails.firstName + userDetails.lastName"
-			p_delivery_address = "userDetails.address"
-			p_delivery_city = "Delhi"
-			p_delivery_state = "Delhi"
-			p_delivery_zip = "110085"
-			p_delivery_country = "India"
-			#p_delivery_tel =userDetails.phoneNo
-			p_merchant_param1 = "userDetails.email"
-			p_merchant_param2 = ""
-			p_merchant_param3 = ""
-			p_merchant_param4 = ""
-			p_merchant_param5 = ""
- 			p_promo_code = ""
-			p_customer_identifier = ""
+		p_order_id = product.productId
+		p_currency = "INR"
+		p_amount = product.price
+		p_redirect_url = "https://www.petalsart.in/ccavResponseHandler/"
+		p_cancel_url = "https://www.petalsart.in/ccavResponseHandler/"
+		p_language = "EN"
+		p_billing_name = "userDetails.firstName" + ' ' + "userDetails.lastName"
+		p_billing_address = "userDetails.address"
+		p_billing_city = "Delhi"
+		p_billing_state = "Delhi"
+		p_billing_zip = "110085"
+		p_billing_country = "India"
+		#p_billing_tel = userDetails.phoneNo
+		p_billing_email = "userDetails.email"
+		p_delivery_name = "userDetails.firstName + userDetails.lastName"
+		p_delivery_address = "userDetails.address"
+		p_delivery_city = "Delhi"
+		p_delivery_state = "Delhi"
+		p_delivery_zip = "110085"
+		p_delivery_country = "India"
+		#p_delivery_tel =userDetails.phoneNo
+		p_merchant_param1 = "userDetails.email"
+		p_merchant_param2 = ""
+		p_merchant_param3 = ""
+		p_merchant_param4 = ""
+		p_merchant_param5 = ""
+ 		p_promo_code = ""
+		p_customer_identifier = ""
 	
 	
 
-			merchant_data='merchant_id='+p_merchant_id+'&'+'order_id='+p_order_id + '&' + "currency=" + p_currency + '&' + 'amount=' + p_amount+'&'+'redirect_url='+p_redirect_url+'&'+'cancel_url='+p_cancel_url+'&'+'language='+p_language+'&'+'billing_name='+p_billing_name+'&'+'billing_address='+p_billing_address+'&'+'billing_city='+p_billing_city+'&'+'billing_state='+p_billing_state+'&'+'billing_zip='+p_billing_zip+'&'+'billing_country='+p_billing_country+'&'+'billing_email='+p_billing_email+'&'+'delivery_name='+p_delivery_name+'&'+'delivery_address='+p_delivery_address+'&'+'delivery_city='+p_delivery_city+'&'+'delivery_state='+p_delivery_state+'&'+'delivery_zip='+p_delivery_zip+'&'+'delivery_country='+p_delivery_country+'&'+'merchant_param1='+p_merchant_param1+'&'+'merchant_param2='+p_merchant_param2+'&'+'merchant_param3='+p_merchant_param3+'&'+'merchant_param4='+p_merchant_param4+'&'+'merchant_param5='+p_merchant_param5+'&'+'promo_code='+p_promo_code+'&'+'customer_identifier='+p_customer_identifier+'&'
+		merchant_data='merchant_id='+p_merchant_id+'&'+'order_id='+p_order_id + '&' + "currency=" + p_currency + '&' + 'amount=' + p_amount+'&'+'redirect_url='+p_redirect_url+'&'+'cancel_url='+p_cancel_url+'&'+'language='+p_language+'&'+'billing_name='+p_billing_name+'&'+'billing_address='+p_billing_address+'&'+'billing_city='+p_billing_city+'&'+'billing_state='+p_billing_state+'&'+'billing_zip='+p_billing_zip+'&'+'billing_country='+p_billing_country+'&'+'billing_email='+p_billing_email+'&'+'delivery_name='+p_delivery_name+'&'+'delivery_address='+p_delivery_address+'&'+'delivery_city='+p_delivery_city+'&'+'delivery_state='+p_delivery_state+'&'+'delivery_zip='+p_delivery_zip+'&'+'delivery_country='+p_delivery_country+'&'+'merchant_param1='+p_merchant_param1+'&'+'merchant_param2='+p_merchant_param2+'&'+'merchant_param3='+p_merchant_param3+'&'+'merchant_param4='+p_merchant_param4+'&'+'merchant_param5='+p_merchant_param5+'&'+'promo_code='+p_promo_code+'&'+'customer_identifier='+p_customer_identifier+'&'
 		
-			encryption = encrypt(merchant_data,workingKey)
+		encryption = encrypt(merchant_data,workingKey)
 
 		return render(request, 'home/payment.html', {'encReq':encryption,'xscode':accessCode})
 
